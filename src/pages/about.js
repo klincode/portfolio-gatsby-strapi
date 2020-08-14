217d5a39238d46f19d1e52462e690edb4717064a
Adam Klin <adam.klin@assecobs.pl>
2020-08-14 12:55:40 +0200

    about ok

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,8 +5,26 @@ import Title from "../components/Title"
 import Image from "gatsby-image"
 // ...GatsbyImageSharpFluid
 const About = ({ data: { about: { nodes } } }) => {
-  console.log(nodes[0]);
-  return <Layout>d</Layout>
+  const { info, stack, title, image } = nodes[0]
+  return <Layout>
+    <section className="about-page">
+      <div className="about-center section-center">
+        <Image fluid={image.childImageSharp.fluid} className="about-img" />
+
+        <article className="about-text">
+          <Title title={title} />
+          <p>{info}</p>
+          <div className="about-stack">
+            {stack.map((item) => {
+              return (
+                <span key={item.id}>{item.title}</span>
+              )
+            })}
+          </div>
+        </article>
+      </div>
+    </section>
+  </Layout>
 }
 
 export const query = graphql`
