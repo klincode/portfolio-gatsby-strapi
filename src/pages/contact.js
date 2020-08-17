4965849dee2091dc7d704b17b9c271f2bfb09fa8
Adam Klin <adam.klin@assecobs.pl>
2020-08-17 18:16:55 +0200

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,22 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return <Layout>
+    <section className="contact-page">
+      <article className="contact-form">
+        <h3>get in touch
+        <form action="https://formspree.io/mvownplg" method="POST">
+            <div className="form-group">
+              <input type="text" placeholder="name" className="form-control" />
+              <input type="email" placeholder="email" className="form-control" />
+              <textarea name="message" placeholder="message" className="form-control"></textarea>
+              <button type="submit" className="submit-btn btn">Submit here</button>
+            </div>
+          </form>
+        </h3>
+      </article>
+    </section>
+  </Layout>
 }
 
 export default contact
